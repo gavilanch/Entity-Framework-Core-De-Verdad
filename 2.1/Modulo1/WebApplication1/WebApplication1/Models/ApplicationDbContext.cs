8913f7366d6b237c307860c906dcbf6bd7b32885
Felipe Gavilan <elmejor_887@hotmail.com>
2019-08-08 09:38:50 -0400

    Agregado WebApplication1 - Modulo 1

---
+++ 2.1/Modulo1/WebApplication1/WebApplication1/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+                : base(options)
+        {
+        }
+    }
+}
